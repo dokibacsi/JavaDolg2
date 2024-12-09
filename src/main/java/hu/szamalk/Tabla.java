24cbe9571246bdf6891c74ecc7589d861344c8ed
dokibacsi <kvalcsi02@gmail.com>
2024-12-09 13:45:28 +0100

    f1 - 3

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,6 +1,17 @@
 package hu.szamalk;
 
 public class Tabla {
-    private int[][] T;
+    private char[][] T;
     private char uresCella;
+
+    public Tabla(char uresCella) {
+        T = new char[8][8];
+        this.uresCella = uresCella;
+
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                T[i][j] = this.uresCella;
+            }
+        }
+    }
 }
